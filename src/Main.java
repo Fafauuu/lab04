a235d739d57e2f8f1f980ec938574a49cc019338
rafał <259146@student.pwr.edu.pl>
2021-11-23 17:36:47 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import gui.GuiFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        var gui = new GuiFrame();
+    }
+}
---
+++ src/model/Receipt.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receipt {
+    List<Ingredient> ingredientList;
+
+    public Receipt() {
+        this.ingredientList = new ArrayList<>();
+    }
+}
+;
